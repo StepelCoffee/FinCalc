d84bdd163a7c42e1d6d7b8936820ae1df5f5b40c
stefa <stefanwogatzki@gmail.com>
2022-05-11 19:10:50 +0200

    annuity and unequal cash flows

--- CashFlowSeries/__init__.py
+++ CashFlowSeries/__init__.py
@@ -10,6 +10,9 @@ class Annuity:
     def calc_final_value(self) -> float:
         return self.__amount*((1+self.__rate)**self.__time_periods-1)/self.__rate
 
+    def calc_present_value(self) -> float:
+        return self.__amount*(1-1/(1+self.__rate)**self.__time_periods)/self.__rate
+
 
 class UnequalCashFlow:
     def __init__(self,amount: List[float], rate_per_period: float, periods: int):
@@ -24,9 +27,22 @@ class UnequalCashFlow:
         return total
 
 
+class LumpSum:
+    def __init__(self, future_value: float, rate_per_period: float, periods: int):
+        self.__future_value = future_value
+        self.__rate = rate_per_period
+        self.__time_periods = periods
+
+    def calc_pv(self) -> float:
+        return self.__future_value / (1+self.__rate)**self.__time_periods
+
+
 
 if __name__ == '__main__':
-    ann = Annuity(20000,0.09,30)
+    ann = Annuity(1000,0.12,5)
     print(ann.calc_final_value())
-    ucf = UnequalCashFlow([1000,2000,4000,5000,6000],0.05,5)
+    print(ann.calc_present_value())
+    ucf = UnequalCashFlow([0,4000,0,8000,0,7000,0,10000],0.02,8)
     print(ucf.calc_final_value())
+    ls = LumpSum(5000,0.05/12,3*12)
+    print(ls.calc_pv())
