2af3fb8e67cd26d704441899505f3105e3be956d
stefa <stefanwogatzki@gmail.com>
2022-05-21 20:23:55 +0200

    provide an example to TwoAssetPortfolio

--- PortfolioTheory/TwoAssetPortfolio.py
+++ PortfolioTheory/TwoAssetPortfolio.py
@@ -32,12 +32,3 @@ class TwoAssetPortfolio:
 def cov_to_corr(cov:float, risks:List[float]) -> float:
     return cov/math.prod(risks)
 
-
-weights = [0.8,0.2]
-risks = [0.1621,0.3311]
-returns = [0.0993,0.1820]
-cov = 0.005
-
-pf = TwoAssetPortfolio(weights, risks, returns, cov_to_corr(cov,risks))
-print(pf.get_portfolio_return())
-print(pf.get_portfolio_risk())
---
+++ PortfolioTheory/TwoAssetsExample.py
@@ -0,0 +1,11 @@
+import TwoAssetPortfolio as tp
+
+if __name__ == '__main__':
+    weights = [0.8,0.2]
+    risks = [0.1621,0.3311]
+    returns = [0.0993,0.1820]
+    cov = 0.005
+
+    pf = tp.TwoAssetPortfolio(weights, risks, returns, tp.cov_to_corr(cov,risks))
+    print(pf.get_portfolio_return())
+    print(pf.get_portfolio_risk())
