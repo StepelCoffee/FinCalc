ce3268e169397e4a3292cde0ba50dd5857cfba36
stefa <stefanwogatzki@gmail.com>
2022-05-21 20:20:22 +0200

    add source to external code

--- Calculus/NumericalRoots/__init__.py
+++ Calculus/NumericalRoots/__init__.py
@@ -1,4 +1,6 @@
 """
+Code Source: https://gist.github.com/np-8/88713c0727af473bc8ac990811f0a446
+
 Simple zero/root finder for functions with one variable.
 
 Finds all the zeroes within a range of values, using user-defined searchstep and tolerance.
@@ -52,31 +54,3 @@ class RootFinder:
         if ier == 1:
             return x[0]
         return None
-
-
-def f(u, v=90, ell=5):
-    # Example from https://stackoverflow.com/q/14878110/3015186
-    w = np.sqrt(v ** 2 - u ** 2)
-
-    jl = scipy.special.jn(ell, u)
-    jl1 = scipy.special.yn(ell - 1, u)
-    kl = scipy.special.kn(ell, w)
-    kl1 = scipy.special.kn(ell - 1, w)
-
-    return jl / (u * jl1) + kl / (w * kl1)
-
-
-if __name__ == "__main__":
-
-    r = RootFinder(1, 20, 0.01)
-    args = (90, 5)
-    roots = r.find(f, *args)
-    print("Roots: ", roots)
-
-    # plot results
-    u = np.linspace(1, 20, num=600)
-    fig, ax = plt.subplots()
-    ax.plot(u, f(u, *args))
-    ax.scatter(roots, f(np.array(roots), *args), color="r", s=10)
-    ax.grid(color="grey", ls="--", lw=0.5)
-    plt.show()
